0da4a365997112d80c58aca92e3adc46269efa94
Mervyn Vangenechten <mervynvangenechten@gmail.com>
2023-06-07 16:32:07 +0200

    -all

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-AOS.init();
-
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
-  
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
